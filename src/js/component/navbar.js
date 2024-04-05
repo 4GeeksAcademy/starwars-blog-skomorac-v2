e9c2cc1f67d2cb596a872557a65eb79f8edec47a
Admir Skomorac <admir.skomorac@outlook.com>
2024-04-05 18:44:54 +0000

    added logo

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../../img/logo.png";
+import ("../../styles/index.css")
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-light mb-3">
+      <Link to="/">
+        <img
+          src={logo}
+          width="80"
+          height="50"
+          className="d-inline-block align-top logo"
+          alt="Logo"
+        />
+      </Link>
+      <div className="ml-auto">
+        <Link to="/demo">
+          <button className="btn btn-primary">
+            Check the Context in action
+          </button>
+        </Link>
+      </div>
+    </nav>
+  );
 };
